6bd03c508851b26390a9b1e441de0842673f14ff
Bob Brown <robertbernardbrown@gmail.com>
2018-09-18 14:26:45 -0400

    adds some initial styling and renders beginning Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+const sizes = {
+  desktop: 992,
+  tablet: 768,
+  phone: 576,
+}
+
+// Iterate through the sizes and create a media template
+const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (min-width: ${sizes[label]}px) {
+      ${css(...args)}
+    }
+  `
+  return acc
+}, {})
 
 const Title = styled.h1`
-  font-size: 1.5em;
+  font-size: 3em;
   text-align: center;
-  color: palevioletred;
+  color: green;
+  ${media.phone`
+  font-size: 5em`}
+  ${media.tablet`
+  font-size: 5em`}
+  ${media.desktop`
+  font-size: 5em`}
 `;
 
 const Header = styled.header`
-  padding: 4em;
-  background: papayawhip;
+  padding: 2em;
 `;
 
 class App extends Component {
@@ -17,7 +38,7 @@ class App extends Component {
       return (
         <Header>
           <Title>
-            Hello World, this is my first styled component!
+            Allowance
           </Title>
         </Header>
       );
